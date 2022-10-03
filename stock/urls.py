eabf1940c667a522fa257981137330322481b9e0
biletweb <bilet.igor@gmail.com>
2022-10-03 16:14:47 +0300

    Implemented sorting by category

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from stock.views import ProductList
+from stock.views import ProductList, CategoryDetail
 
 urlpatterns = [
     path('', ProductList.as_view(), name='stock_list'),
+    path('category/<slug:slug>/', CategoryDetail.as_view(), name='stock_category'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,7 @@
 from stock.models import Stock, Category
 from django.views.generic import ListView
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
 
 class ProductList(ListView):
@@ -18,3 +19,23 @@ class ProductList(ListView):
 
     def get_queryset(self):
         return Stock.objects.filter(availability=True)
+
+
+class CategoryDetail(ListView):
+    model = Stock  # model for work
+    template_name = 'stock/category_product.html'  # default sklad_list.html
+    context_object_name = 'stock'  # default objects_list
+    ordering = ['-added_at']
+    paginate_by = 3  # number of posts per page
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_category_info = Category.objects.get(slug=self.kwargs['slug'])
+        context['title'] = current_category_info.title
+        context['categories'] = Category.objects.annotate(cnt=Count('stock')).filter(cnt__gt=0)
+        context['current_category_info'] = current_category_info
+        return context
+
+    def get_queryset(self):
+        category_id = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Stock.objects.filter(category=category_id.pk, availability=True)
